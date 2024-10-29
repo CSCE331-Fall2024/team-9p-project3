71314b59d2941913dadf60df6c53f604162b5c20
trumanharp-tamu <trumanharp@tamu.edu>
2024-10-29 16:31:18 -0500

    order start page

--- project3/src/app/customerStart/page.js
+++ project3/src/app/customerStart/page.js
@@ -2,10 +2,15 @@ import Link from "next/link";
 
 export default function CustomerStartPage() {
     return (
-        <main>
-            <Link href="/">
+        <div className="flex flex-col justify-center items-center h-screen">
+            <div className="flex flex-1 justify-center items-center w-full bg-red-700">
+                <h1 className="text-8xl font-medium text-red-100">
+                    Welcome to Panda Express!
+                </h1>
+            </div>
+            <Link href="/" className="flex justify-center items-center w-full h-1/3 bg-red-300 text-black text-2xl hover:bg-red-400 transition-colors">
             GO BACK ??
             </Link>
-        </main>
+        </div>
     );
 }
