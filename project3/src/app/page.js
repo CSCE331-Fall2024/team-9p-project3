78cbb6ae20aa1623273c82f55ddc0fde272eea2f
trumanharp-tamu <trumanharp@tamu.edu>
2024-10-29 16:37:52 -0500

    commented out stock page.js stuff

--- project3/src/app/page.js
+++ project3/src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Gallery></Gallery>
+        {/* <Gallery></Gallery>
         <Image
           className="dark:invert"
           src="/next.svg"
@@ -24,11 +24,12 @@ export default function Home() {
             .
           </li>
           <li>Save and see your changes instantly.</li>
-        </ol>
+        </ol> */}
         <div className="">
-          <Link href="/customerStart">Test Link</Link>
+          <h1 className="text-5xl">Log In Goes Here</h1>
+          <Link href="/customerStart" className="text-5xl hover:text-red-400">Launch Customer View</Link>
         </div>
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
+        {/* <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
@@ -100,7 +101,8 @@ export default function Home() {
           />
           Go to nextjs.org →
         </a>
-      </footer>
+      </footer> */}
+      </main>
     </div>
   );
 }
