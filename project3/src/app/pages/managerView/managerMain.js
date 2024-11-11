b22546e48e3815f07eea6466958c51f0b3454e6f
Mario Torres <metorresramos@gmail.com>
2024-11-11 09:40:52 -0600

    updated layout of customer start

--- project3/src/app/pages/managerView/managerMain.js
+++ project3/src/app/pages/managerView/managerMain.js
@@ -5,13 +5,65 @@ export default function ManagerMainPage({ switchPage }) {
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen p-8 bg-gray-100">
-            <h1 className="text-4xl font-bold mb-8">Manager Main</h1>
-            <button
-                onClick={handleGoBack}
-                className="p-4 bg-red-500 text-white rounded-md hover:bg-red-600"
-            >
-                Go Back to Login
-            </button>
+            <h1 className="text-4xl font-bold mb-8">Manager View</h1>
+            
+            <div className="flex flex-col gap-4 w-1/2">
+                <button
+                    onClick={() => switchPage('employeesPage')}
+                    className="p-4 bg-red-500 text-white rounded-md hover:bg-red-600"
+                >
+                    Employees
+                </button>
+                
+                <button
+                    onClick={() => switchPage('menuItemsPage')}
+                    className="p-4 bg-red-500 text-white rounded-md hover:bg-red-600"
+                >
+                    Menu Items
+                </button>
+                
+                <button
+                    onClick={() => switchPage('inventoryPage')}
+                    className="p-4 bg-red-500 text-white rounded-md hover:bg-red-600"
+                >
+                    Inventory
+                </button>
+                
+                <button
+                    onClick={() => switchPage('orderHistoryPage')}
+                    className="p-4 bg-red-500 text-white rounded-md hover:bg-red-600"
+                >
+                    Order History
+                </button>
+                
+                <button
+                    onClick={() => switchPage('xReportPage')}
+                    className="p-4 bg-red-500 text-white rounded-md hover:bg-red-600"
+                >
+                    X Report
+                </button>
+                
+                <button
+                    onClick={() => switchPage('zReportPage')}
+                    className="p-4 bg-red-500 text-white rounded-md hover:bg-red-600"
+                >
+                    Z Report
+                </button>
+                
+                <button
+                    onClick={() => switchPage('usageChartPage')}
+                    className="p-4 bg-red-500 text-white rounded-md hover:bg-red-600"
+                >
+                    Usage Chart
+                </button>
+
+                <button
+                    onClick={handleGoBack}
+                    className="p-4 bg-red-500 text-white rounded-md hover:bg-red-600 mt-9"
+                >
+                    Go Back to Login
+                </button>
+            </div>
         </div>
     );
 }
