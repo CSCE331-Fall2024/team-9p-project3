71ba39ad5ac8797671b9708dcf785b043f5d124c
Mario Torres <metorresramos@gmail.com>
2024-11-11 09:42:49 -0600

    start screen update

--- project3/src/app/pages/start.js
+++ project3/src/app/pages/start.js
@@ -4,12 +4,12 @@ export default function StartPage({ switchPage }) {
         <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
             <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
                 <div className="">
-                <h1 className="text-5xl cursor-pointer hover:text-blue-500" onClick={() => switchPage('loginPage')}>
+                <h1 className="text-5xl cursor-pointer hover:text-red-5 00 m-5" onClick={() => switchPage('loginPage')}>
                         Log In Goes Here
                     </h1>
                     <button
                         onClick={() => switchPage('customerStartPage')}
-                        className="text-5xl hover:text-red-400"
+                        className="text-5xl hover:text-red-500 m-5"
                     >
                         Launch Customer View
                     </button>
