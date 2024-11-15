bf26c969b0d001a60e417cd7c243ab07500d8a84
Kyle Easton <kyleeaston@tamu.edu>
2024-11-15 08:51:39 -0600

    login api, employee api framework

---
+++ project3/src/app/pages/api/getemployee/route.js
@@ -0,0 +1,20 @@
+import { query } from '../dbconn';
+import { NextRequest, NextResponse } from "next/server";
+
+
+export async function GET(req) {
+    console.log('inside handler');
+    if (req.method === 'GET') {
+        try {
+            const result = await query('SELECT * FROM employees;'); 
+            return NextResponse.json(result);
+        } catch (error) {
+            // console.error("Query error:", error);
+            // res.status(500).json({ error: 'Database query failed' });
+            console.error('get better: error');
+        }
+    } else {
+        // res.setHeader('Allow', ['GET']);
+        // res.status(405).end(`Method ${req.method} Not Allowed`);
+    }
+}
--- project3/src/app/pages/loginPage.js
+++ project3/src/app/pages/loginPage.js
@@ -1,27 +1,36 @@
-import { useState } from 'react';
-import { query } from './api/dbconn';
+import { useState, useEffect } from 'react';
+// import { query } from './api/dbconn';
 
 export default function LoginPage({ switchPage }) {
-
+    // return(
+    //     <div>
+    //         <p>Hello</p>
+    //     </div>
+    // );
     const [username, setUsername] = useState('');
     // const [password, setPassword] = useState('');
     const [error, setError] = useState('');
 
-    const handleLogin = async () => {
+        const handleLogin = async() => {
+            try{
+                const response = await fetch(`./pages/api/login?username=${username}`);
+                const isValid = await response.json()
+                if (!response.ok) {
+                    throw new Error(`Error: ${response.status} - ${response.statusText}`);
+                }
+                console.log(isValid.valid)
+                if (isValid.valid) {
+                    switchPage('managerMainPage');
+                } else {
+                    setError('Invalid username or Password');
+                }
+            }
+            catch (error){
+                console.error('Error in fetch:', error)
+            }
+        };
 
-        const result = await query('SELECT COUNT(*) FROM employees WHERE name = $1', [username]);
-        if (parseInt(result.rows[0].count, 10) > 0) {
-            switchPage('managerMainPage');
-        } else {
-            setError('Invalid username or Password');
-        }
 
-        // if (username === 'manager') {
-        //     switchPage('managerMainPage');
-        // } else {
-        //     setError('Invalid username or Password');
-        // }
-    };
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen p-8 bg-gray-100">
