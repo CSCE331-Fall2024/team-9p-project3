c62500ebafa7c6769346df93038f3c5b8d0d1d54
Mario Torres <metorresramos@gmail.com>
2024-12-05 19:42:27 -0600

    employee view changes

--- project3/src/app/pages/customerView/appetizerPage.js
+++ project3/src/app/pages/customerView/appetizerPage.js
@@ -6,12 +6,12 @@ export default function AppetizerPage({cart, switchPage}) {
     return (
         <main className="flex flex-col justify-center bg-gray-200 items-center h-screen">
             <OrderingHeader cart={cart} switchPage={switchPage}/>
-                <div className="flex flex-col justify-center items-end h-full w-full">
-                    <OrderingTopPanel title="Choose an Appetizer"/>
-                    <div className="flex justify-center items-center h-5/6 w-full">
-                        <AppetizerSelector cart={cart} switchPage={switchPage} numRequired={1}/>
-                    </div>
+            <div className="flex flex-col justify-center items-end h-full w-full">
+                <OrderingTopPanel title="Choose an Appetizer"/>
+                <div className="flex justify-center items-center h-5/6 w-full">
+                    <AppetizerSelector cart={cart} switchPage={switchPage} numRequired={1}/>
                 </div>
+            </div>
             <OrderingFooter switchPage={switchPage} cart={cart}/>
         </main>
     );
--- project3/src/app/pages/employeeView/appetizerPage.js
+++ project3/src/app/pages/employeeView/appetizerPage.js
@@ -4,15 +4,12 @@ import { CartSidePanel, OrderingHeader, OrderingTopPanel, MenuItemButton, Orderi
 export default function EmployeeAppetizerPage({cart, switchPage}) {
     var topPanelString = "Choose ";
     return (
-        <main className="flex flex-col justify-center items-center h-screen">
+        <main className="flex flex-col justify-center bg-gray-200 items-center h-screen">
             <OrderingHeader cart={cart} switchPage={switchPage} employee={true}/>
-            <div className="flex flex-row h-full w-full">
-                <CartSidePanel cart={cart}/>
-                <div className="flex flex-col justify-center items-end h-full w-full">
-                    <OrderingTopPanel title="Choose an Appetizer"/>
-                    <div className="flex justify-center items-center h-5/6 w-full">
-                        <AppetizerSelector cart={cart} switchPage={switchPage} numRequired={1} employee={true}/>
-                    </div>
+            <div className="flex flex-col justify-center items-end h-full w-full">
+                <OrderingTopPanel title="Choose an Appetizer"/>
+                <div className="flex justify-center items-center h-5/6 w-full">
+                    <AppetizerSelector cart={cart} switchPage={switchPage} numRequired={1} employee={true}/>
                 </div>
             </div>
             <OrderingFooter switchPage={switchPage} cart={cart} employee={true}/>
