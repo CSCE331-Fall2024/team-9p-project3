35fe958ee3627938457e17c20478d0869951d4d4
Yuan Wang <yuanw1010@tamu.edu>
2024-12-11 15:14:58 -0600

    Code documentataion 2

--- project3/src/app/components/AppetizerSelector.js
+++ project3/src/app/components/AppetizerSelector.js
@@ -1,15 +1,27 @@
 import { useState, useEffect } from 'react';
 import { CartObject} from '../objects/cartObject';
 
+// Defines a React component called AppetizerSelector. 
+// Used for showing appetizers in Appetizer select page. 
 export default function AppetizerSelector({ cart, switchPage, numRequired, employee=false}) {
+
+    // Declare state variables and functions to update it. 
+    // The initial value of each state variables is set by useState. 
     const [selectedItems, setSelectedItems] = useState([]);
     const [items, setItems] = useState([]);
 
+    // useEffect defines and runs a function named fetchApps. 
     useEffect(() => {
+        // This fetchApps function sends a GET request, get the return value from the server, and update the state variable named items. 
         const fetchApps = async () => {
             try {
+                // Console message. Used for checking whether sending the request is successful. 
                 console.log('inside fetch entrees')
+
+                // Send the GET request to this API endpoint and save the return value to the variable called response. 
                 const response = await fetch('./pages/api/appetizer');
+
+                // Console message
                 console.log('after await');
                 
                 if (!response.ok) {
@@ -17,37 +29,49 @@ export default function AppetizerSelector({ cart, switchPage, numRequired, emplo
                 }
     
                 const data = await response.json();
+
+                // Update the state variable items. 
                 setItems(data.rows.map(row => row.name));
                 console.log(items);
-                // return data.rows;
-                // return items;
             } catch (error) {
                 console.error('Fetch error:', error);
             }
         };
         console.log('about to fetch');
+        
+        // run above fetchApps function
         fetchApps();
 
         
-      }, []); 
+      }, []); // [] ensures that useEffect runs only once. 
       console.log(items);
 
     let numSelect = parseInt(numRequired);
-    
+
+    // toggleSelection function implements select and deselect functions when customers select appetizers in the Appetizer interface. 
     const toggleSelection = (item) => {
+
+        // Deselect: 
+        // If the item is already in the selectedItems list, then remove it. 
         if (selectedItems.includes(item)) {
             setSelectedItems(selectedItems.filter(selected => selected !== item));
         } 
+
+        // Select:  
+        // If the item isn't in the list and the list doesn't reach the max length, then add it to the list. 
         else if (selectedItems.length < numSelect) {
             setSelectedItems([...selectedItems, item]);
         }
     };
+
+    // handleSelection function replaces the entire selectedItems list with the argument item. 
     const handleSelection = (item) => {
         if (!selectedItems.includes(item)) {
             setSelectedItems([item]);
         }
     };
 
+    // handleSubmit function adds the selected Appetizer to customer's cart. 
     const handleSubmit = () => {
         const newCartObject = new CartObject(selectedItems.at(0));
         cart.addItem(newCartObject)
@@ -59,6 +83,7 @@ export default function AppetizerSelector({ cart, switchPage, numRequired, emplo
         }
     };
 
+    // This function makes and styles buttons
     const buttonClassName = (item, selectedItems) => {
         if (selectedItems.includes(item)) {
             return "p-4 text-2xl text-black rounded font-semibold transition-colors bg-red-600";
