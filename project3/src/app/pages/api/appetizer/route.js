6e3d245c054a94de9010d3dd426df26b2739c66e
Yuan Wang <yuanw1010@tamu.edu>
2024-12-11 15:18:07 -0600

    code documentation 3

--- project3/src/app/pages/api/appetizer/route.js
+++ project3/src/app/pages/api/appetizer/route.js
@@ -1,13 +1,20 @@
+// import the query function from our dbconn file. 
 import { query } from '../dbconn';
 import { NextRequest, NextResponse } from "next/server";
 
-
+// This function handles the HTTP GET request. 
 export async function GET(req) {
+    // Console message. Used for checking whether get into the API endpoint sucessfully. 
     console.log('inside handler');
+
+    // Check if the request is POST. 
     if (req.method === 'GET') {
         try {
+            // Executes the Database query using query() function and saves its return value to the variable called result.
             const result = await query('SELECT * FROM servable_items WHERE type = 3;'); 
             return NextResponse.json(result);
+
+        // catch the error and return the error message if any exception above.
         } catch (error) {
             // console.error("Query error:", error);
             // res.status(500).json({ error: 'Database query failed' });
